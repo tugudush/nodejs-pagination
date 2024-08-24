d7f7073fbf057afbcac3eabc16f596caf21838a9
tugudush <jerome2kph@gmail.com>
2024-08-24 13:12:07 +0800

    initial commit

---
+++ error-playground.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => {
+  if (a && b) {
+    return a + b;
+  }
+  throw new Error("Invalid arguments");
+};
+
+try {
+  console.log(sum(1));
+} catch (error) {
+  console.log("Error occured!");
+}
+
+//console.log(sum(1));
+console.log('this works!')
